860138d2f33d8110e6e93a74225d0af2b2d6241c
MMendes24 <mmbusiness1@protonmail.com>
2020-08-18 15:05:33 -0400

    Basic form completed.

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialFormValues = {
+    name: "",
+    age: "",
+    email: "",
+}
 
 const Login = () => {
+    const [ formValues, setFormValues] = useState(initialFormValues)
+
+    const onFormChange = e => {
+        e.preventDefault()
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <h1>Login Temp</h1>
+        <section>
+            <h1>New Friend</h1>
+            <form>
+                <label>Name:&nbsp;
+                    <input 
+                    name="name"
+                    type="text"
+                    value={formValues.name}
+                    onChange={onFormChange}                    
+                    />
+                </label>
+                <label>Age:&nbsp;
+                    <input 
+                    name="age"
+                    type="text"
+                    value={formValues.age}
+                    onChange={onFormChange}                      
+                    />
+                </label>
+                <label>Email:&nbsp;
+                    <input 
+                    name="email"
+                    type="text"
+                    value={formValues.email}
+                    onChange={onFormChange}                      
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </section>
     )
 }
 
